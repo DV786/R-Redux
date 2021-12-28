758ff380629ef060b0542db01e47e1dbcfec27fe
DV786 <vdarshan786@gmail.com>
2021-12-28 18:47:16 +0530

    first commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
             <div className='addTocart'>
                 <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" alt="Cart" />
             </div>
-            <h1>Hom Component</h1>
+            <h1>Home Component</h1>
             <div className='cart-wrapper'>
                 <div className='img-wrapper item'>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6U3V_MBKP9OpwTFOXSjY7Pneh57tISp1XZKevGhU6C6jmb8ay_fkRfVW2-84dcHRDjRU&usqp=CAU" alt="No Img Found" />
